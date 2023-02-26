6766989d4b52dc5acf8af1b155dd987fba9db174
GrigorHovhannisyan98 <120597226+GRIGORHOVHANISYAN@users.noreply.github.com>
2023-02-27 00:37:50 +0400

    swagger

--- src/app.js
+++ src/app.js
@@ -2,8 +2,17 @@ import express from "express";
 import bodyParser from "body-parser";
 import productRoutes from "./routes/product.js";
 import "dotenv/config";
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import fs from "fs"
+
+const rawdata =  fs.readFileSync("./src/swagger.json")
+const swaggerOptions =JSON.parse(rawdata)
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
 
 const app = express();
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
